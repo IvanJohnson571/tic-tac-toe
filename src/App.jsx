1ea03942e6a5dc4a78a7b2a88c4cc1fad66552e4
IvanJohnson571 <ivan_johnson2005@yahoo.com>
2024-09-02 17:59:16 +0300

    Added game over logic

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,14 @@ import { useState } from "react"
 import GameBoard from "./components/GameBoard"
 import Player from "./components/Player"
 import Log from "./components/Log";
+import { WINNING_COMBINATIONS } from "./winning-combinations";
+import GameOver from "./components/GameOver";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 
 function derivedActivePlayer(gameTurns) {
 
@@ -20,8 +28,35 @@ function derivedActivePlayer(gameTurns) {
 function App() {
 
   const [gameTurns, setGameTurns] = useState([]);
-
   const activePlayer = derivedActivePlayer(gameTurns);
+  let gameBoard = [...initialGameBoard.map(array => [...array])];
+  let winner;
+
+  for (const turn of gameTurns) {
+
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+
+  }
+
+
+  for (const combination of WINNING_COMBINATIONS) {
+
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
+
+    if (firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol) {
+
+      winner = firstSquareSymbol;
+
+    }
+
+  }
+
+  const hasDraw = gameTurns.length === 9 && !winner;
 
   function handleSelectSquare(rowIndex, colIndex) {
 
@@ -46,6 +81,10 @@ function App() {
 
   }
 
+  function handleRestart() {
+    setGameTurns([]);
+  }
+
   return (
     <main>
       <div id="game-container">
@@ -54,7 +93,8 @@ function App() {
           <Player initialName='Player 1' symbol='X' isActive={activePlayer === 'X'} />
           <Player initialName='Player 2' symbol='O' isActive={activePlayer === 'O'} />
         </ol>
-        <GameBoard onSelectSuqare={handleSelectSquare} turns={gameTurns} />
+        {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart} />}
+        <GameBoard onSelectSuqare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
     </main>
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,44 +1,9 @@
-import { WINNING_COMBINATIONS } from '../winning-combinations';
 
-const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
-];
-
-export default function GameBoard({ onSelectSuqare, turns }) {
-
-    let gameBoard = initialGameBoard;
-
-    for (const turn of turns) {
-
-        const { square, player } = turn;
-        const { row, col } = square;
-
-        gameBoard[row][col] = player;
-
-    }
-
-    // const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-    // function handleSelectSquare(rowIndex, colIndex) {
-
-    //     setGameBoard((prevGameBoard) => {
-
-    //         const updatedBoard = [...prevGameBoard.map((innerArray) => [...innerArray])];
-    //         updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-
-    //         return updatedBoard;
-
-    //     });
-
-    //     onSelectSuqare();
-
-    // }
+export default function GameBoard({ onSelectSuqare, board }) {
 
     return (
         <ol id="game-board">
-            {gameBoard.map((row, rowIndex) => <li key="rowIndex">
+            {board.map((row, rowIndex) => <li key="rowIndex">
                 <ol>
                     {row.map((playerSymbol, colIndex) =>
                         <li key={colIndex}>
